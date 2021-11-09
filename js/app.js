ebaed2fc45c39c42f162b40bfb6bbfe39aec99d2
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-11-08 20:35:33 -0600

    Udpate appJs

--- js/app.js
+++ js/app.js
@@ -7,17 +7,13 @@ var requestOptions = {
 fetch("https://dog.ceo/api/breeds/image/random/21", requestOptions)
   .then(response => response.json())
   .then(result => showDogs(result))
-  .catch(error => console.log('La conexion con el servicio fallo'));
+  .catch(error => erroConection(error));
 
 
 container = document.querySelector(".container")
 const alert_content = document.querySelector("#alert-content");
 
 const showDogs = (result) => {
-  if (result == "" || result == null || !result ){
-      Console.log('La conexion con el servicio fallo');
-  } else {
-    alert_content.classList.add("d-none");
     for (let i = 0; i < result.message.length; i++) {
       const movieElement = document.createElement('div');
       movieElement.classList.add('col');
@@ -31,5 +27,11 @@ const showDogs = (result) => {
      
         `;
         main.appendChild(movieElement);
-      }}
+      }
+  }
+
+ function erroConection(error){
+    if(error){
+      alert_content.classList.remove("d-none");
+    }
   }
