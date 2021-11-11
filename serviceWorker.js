4d5e863c72564acea093f03497a59f204c9c7cdf
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-11-11 14:33:39 -0600

    Corrección de Errores V1

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,44 @@
-const cacheName = "AdoptDogv4";
-const _assets = [
+//const cacheName = "AdoptDogv1.14";
+
+let staticCache = 'staticCache_v1';
+let dynamicCache = 'dynamicCache_v1';
+let immutableCache = 'immutableCache_v1'; 
+ 
+const _staticFiles = [
   "/",
   "/index.html",
-  "/css/style.css",
-  "/css/bootstrap.min.css",
   "/images/dog_icon.svg",
   "/images/alert-dog.png",
+  "/css/style.css",
   "/js/app.js",
   "/js/main.js",
+];
+
+const _immutableFiles = [
+  "/css/bootstrap.min.css",
+  "/css/bootstrap.min.css.map",
   "/js/jquery.js",
   "/js/party.js",
   "/js/bootstrap.bundle.min.js",
+  "/js/bootstrap.bundle.min.js.map"
 ];
 
 self.addEventListener("install", (installEvent) => {
-  const _app_shell = caches
-    .open(cacheName)
-    .then((cache) => cache.addAll(_assets));
+  const saveStaticCache = caches
+    .open(staticCache)
+    .then((cache) => cache.addAll(_staticFiles));
 
-  installEvent.waitUntil(_app_shell);
+  const saveImmutableCache = caches
+    .open(immutableCache)
+    .then((cache) => cache.addAll(_immutableFiles));
+
+    installEvent.waitUntil(
+      Promise.all(
+        [
+          saveStaticCache, 
+          saveImmutableCache
+        ])
+    );
 
 });
 
@@ -27,7 +47,7 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((keys) =>
       Promise.all(
         keys.map((key) => {
-          if (!cacheName.includes(key)) {
+          if (!staticCache.includes(key) && !immutableCache.includes(key) ) {
             return caches.delete(key);
           }
         })
@@ -37,14 +57,34 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (fetchEvent) => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then((res) => {
-      return (
-        res ||
-        fetch(fetchEvent.request).catch((error) =>
-          console.log("Error en la peticion")
-        )
-      );
-    })
-  );
+  //fetchEvent.respondWith(
+    //caches.match(fetchEvent.request).then((res) => {
+     // return (
+     //   res ||
+      //  fetch(fetchEvent.request).catch((error) =>
+       //   console.log("Error en la peticion")
+       // )
+     // );
+   // })
+ // );
+ const _result = caches.match(fetchEvent.request).then((cacheResponse) =>{
+  return cacheResponse || fetch(fetchEvent.request).then(
+    networkResponse => {
+      caches.open(dynamicCache).then(cache => {
+        cache.put(fetchEvent.request, networkResponse.clone())
+        return networkResponse
+      })
+    }
+  )
+})
+
+});
+
+//First cache with backup
+
+
+self.addEventListener('message',(msgClient) => {
+    if(msgClient.data.action == 'skipWaiting'){
+        self.skipWaiting();
+    }
 });
