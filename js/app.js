f5dd34b2eea48e8807008cecad2840f5395669c7
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-11-11 14:49:28 -0600

    v4

---
+++ js/app.js
@@ -0,0 +1,72 @@
+let newServiceWorker;
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then((registerEvent) => {
+        registerEvent.addEventListener("updatefound", () => {
+          newServiceWorker = registerEvent.installing;
+          newServiceWorker.addEventListener("statechange", () => {
+            switch (newServiceWorker.state) {
+              case "installed":
+                showSnackbarUpdate();
+                break;
+            }
+          });
+        });
+      });
+  });
+}
+
+function showSnackbarUpdate() {
+  let x = document.getElementById("snackbar");
+  x.className = "show";
+}
+
+let lauchUpdate = document.getElementById("lauchUpdate");
+
+lauchUpdate.addEventListener("click", () => {
+  newServiceWorker.postMessage({
+    action: "skipWaiting",
+  });
+  window.location.reload();
+});
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+fetch("https://dog.ceo/api/breeds/image/random/21", requestOptions)
+  .then((response) => response.json())
+  .then((result) => showDogs(result))
+  .catch((error) => erroShowAlert(error));
+
+container = document.querySelector(".container");
+const alert_content = document.querySelector("#alert-content");
+
+const showDogs = (result) => {
+
+    for (let i = 0; i < result.message.length; i++) {
+      const movieElement = document.createElement("div");
+      movieElement.classList.add("col");
+      movieElement.innerHTML = `
+
+        
+            <div class="card">
+            <img src=${result.message[i]} class="card-img-top" alt="...">
+                <div class="card-body">
+                   <div class="button btn btn-info" onmousedown="party.confetti(this)">Adoptame!</div>
+                </div>
+           </div>
+     
+        `;
+      main.appendChild(movieElement);
+    }
+  
+};
+function erroShowAlert(error) {
+  if(error) {
+    alert_content.classList.remove("d-none");
+  }
+}
